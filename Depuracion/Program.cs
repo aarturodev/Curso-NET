f6fce323ec91084e05aca7bb8e6f66506819987d
= <=>
2025-06-05 12:38:15 -0500

    Add initial project files for Depuracion with sample data processing

---
+++ Depuracion/Program.cs
@@ -0,0 +1,20 @@
+﻿List<string> listaNombres = new List<string>();
+List<string> listaApellidos = new List<string>();
+
+listaNombres.Add("Anderson");
+listaNombres.Add("Arturo");
+listaNombres.Add("Camilo");
+listaNombres.Add("Sofia");
+
+listaApellidos.Add("Marin");
+listaApellidos.Add("Chaves");
+listaApellidos.Add("Lopez");
+listaApellidos.Add("Rojas");
+
+foreach (string nombre in listaNombres)
+{
+  foreach (string apellido in listaApellidos)
+  {
+    Console.WriteLine($"{nombre} {apellido}");
+  }
+}
