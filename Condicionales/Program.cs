a6c1936e44bd4632d3e55d595a418737089b9d82
= <=>
2025-06-05 09:34:35 -0500

    Add project files and initial implementations for Condicionales, HolaMundo, and ReadWrite projects

---
+++ Condicionales/Program.cs
@@ -0,0 +1,13 @@
+﻿int edad = 17;
+
+
+if (edad >= 18)
+{
+  Console.WriteLine("Eres Mayor de edad");
+}
+else
+{
+  Console.WriteLine("Eres Menor de Edad");
+}
+
+Console.WriteLine("fin del programa");
---
+++ ReadWrite/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Escribe tu nombre: ");
+string? nombre = Console.ReadLine();
+
+Console.Write("Escribe tu edad: ");
+int edad = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Hola " + nombre + " tu edad es " + edad);
