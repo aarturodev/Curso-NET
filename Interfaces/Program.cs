365830ca8773809bd12503b870c43c5646606da7
= <=>
2025-06-05 12:00:09 -0500

    Add CalculatorService implementation and interface

---
+++ Interfaces/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+CalculatorService calculator = new CalculatorService();
+
+Console.WriteLine("Welcome to the Calculator Service!");
+
+Console.WriteLine("Suma: "+ calculator.Add(5, 3));
+Console.WriteLine("Resta: " + calculator.Subtract(5, 3));
+Console.WriteLine("Multiplicación: " + calculator.Multiply(5, 3));
+Console.WriteLine("División: " + calculator.Divide(5, 10));
