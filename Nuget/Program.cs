3722ca645ae0e1f1da4acc45854c028d004c7ec1
= <=>
2025-06-05 21:42:30 -0500

    Add Nuget project with JSON serialization example

---
+++ Nuget/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+public class Person
+{
+  public string? Name { get; set; }
+  public int? Age { get; set; }
+}
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    var person = new Person
+    {
+      Name = "Anderson",
+      Age = 23
+    };
+
+    string json = JsonConvert.SerializeObject(person);
+    Console.WriteLine("Json serializado: " + json);
+
+    var deserializacion = JsonConvert.DeserializeObject<Person>(json);
+    Console.WriteLine($"Json deserializado: Name = {deserializacion!.Name} Age = {deserializacion!.Age}");
+
+  }
+}
